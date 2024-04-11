d38c0a32881bec20c1d45c3728374568017bc19d
a.brenga <antobrg90@hotmail.it>
2024-04-11 23:06:48 +0200

    initial commit

---
+++ src/org/lessons/java/StampaSaluto.java
@@ -0,0 +1,11 @@
+package org.lessons.java;
+
+public class StampaSaluto {
+
+	public static void main(String[] args) {	
+		System.out.println("Hello Java!!!");
+		String nome= "Antonietta";
+		System.out.println("hi "+ nome + " how are you?");
+	}
+
+};
